a38e59400d7211432d469b12074aba70d2c2bbfe
PRITAM CHAKROBORTY <92992609+anynomous001@users.noreply.github.com>
2023-05-16 12:42:11 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -86,17 +86,18 @@ function renderResult(data,isAdded = false){
     <div id="data-div" >
         <img id='poster' src = '${data.Poster}'>
         <div id='details-div'>
-        <div>
-            <h3 id= 'title'  class='${data.imdbID}' >${data.Title}</h3>
-            <span id = 'rating'>${data.imdbRating}</span>
-            <span id='watchlist-span'>Add to watchlist<button class='watchlist-btn' data-movie='${data.imdbID}'>
-            ${isInWatchlist(data.imdbID) ? '-' : '+'}
-            </button></span>
+            <div>
+                <h3 id= 'title'  class='${data.imdbID}' >${data.Title}</h3> 
+                <img id='star-icon' src="data:image/svg+xml;charset=utf8,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 64 64' aria-labelledby='title' aria-describedby='desc' role='img' xmlns:xlink='http://www.w3.org/1999/xlink'%3E%3Ctitle%3EStarred User%3C/title%3E%3Cdesc%3EA line styled icon from Orion Icon Library.%3C/desc%3E%3Cpath data-name='layer2' d='M39.8 51H2s0-7 9.4-8.8S20 38.6 20 37v-2a14.2 14.2 0 0 1-4-7c-2.5 0-4-3-4-6 0-.8 0-4 2-4-1.6-6.4-.4-13 4-13 10.4-7 25-1.5 20 13 2 0 2 3.2 2 4 0 3-1.5 6-4 6a14.1 14.1 0 0 1-4 7v2c0 1.1.5 3 3.9 4l6.8 1.7' fill='none' stroke='%23202020' stroke-miterlimit='10' stroke-width='2' stroke-linejoin='round' stroke-linecap='round'%3E%3C/path%3E%3Cpath data-name='layer1' fill='none' stroke='%23202020' stroke-miterlimit='10' stroke-width='2' d='M46 54.9L36.1 62l3.8-11.5L30 44h12.2L46 32l3.8 12H62l-9.9 6.5L55.9 62 46 54.9z' stroke-linejoin='round' stroke-linecap='round'%3E%3C/path%3E%3C/svg%3E" alt="Starred User" /><span id = 'rating'>${data.imdbRating}</span> <br>
+                <span id='watchlist-span'>Add to watchlist<button class='watchlist-btn' data-movie='${data.imdbID}'>
+                ${isInWatchlist(data.imdbID) ? '-' : '+'}
+                </button></span>
             </div>
-            <div id='runtime-others'><span>${data.Runtime}</span><span>${data.Genre}</span></div>
-            <p id='plot'>${data.Plot}</P>
-        </div>
+                <div id='runtime-others'><span>${data.Runtime}</span><span>${data.Genre}</span></div>
+                <p id='plot'>${data.Plot}</P>
+            </div>
     </div>
+    <hr>
     `
     
     //here i was following onclick=addToWatchlist(this) approach for for getting the imdbId
